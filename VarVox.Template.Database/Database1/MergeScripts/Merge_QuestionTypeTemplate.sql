96a51ce42978a8936499c360fb38b438ffd8d91f
Ibrahim Nasirkhan Pathan <wd980367@varianm365.com>
2024-09-10 16:16:49 +0530

    added template type table

---
+++ VarVox.Template.Database/Database1/MergeScripts/Merge_QuestionTypeTemplate.sql
@@ -0,0 +1,12 @@
+﻿IF OBJECT_ID('tempdb..#QuestionTypeTemplate') Is NOT NULL
+ DROP TABLE #QuestionTypeTemplate
+
+ CREATE TABLE #QuestionTypeTemplate
+ (
+	[Id] INT NOT NULL PRIMARY KEY,
+	[QuestionType] VARCHAR(50) NOT NULL,
+	[QuestionJsonTemp] VARCHAR(MAX) NOT NULL
+ )
+
+ INSERT INTO #QuestionTypeTemplate(Id, QuestionType, QuestionJsonTemp)
+
---
+++ VarVox.Template.Database/Database1/Table/QuestionTypeTemplate.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [Ref].[QuestionTypeTemplate]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[QuestionType] VARCHAR(50) NOT NULL,
+	[QuestionJsonTemp] VARCHAR(MAX) NOT NULL
+
+)
