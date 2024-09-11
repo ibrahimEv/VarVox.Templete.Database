67a7633b8a74c28d0830e3858afdf7fdef93ece3
Ibrahim Nasirkhan Pathan <wd980367@varianm365.com>
2024-09-11 12:26:25 +0530

    Added merge scripts

--- VarVox.Template.Database/Database1/MergeScripts/Merge_QuestionTypeTemplate.sql
+++ VarVox.Template.Database/Database1/MergeScripts/Merge_QuestionTypeTemplate.sql
@@ -6,7 +6,38 @@
 	[Id] INT NOT NULL PRIMARY KEY,
 	[QuestionType] VARCHAR(50) NOT NULL,
 	[QuestionJsonTemp] VARCHAR(MAX) NOT NULL
+	[IsActive] BIT NOT NUll
  )
 
- INSERT INTO #QuestionTypeTemplate(Id, QuestionType, QuestionJsonTemp)
+ INSERT INTO #QuestionTypeTemplate(Id, QuestionType, QuestionJsonTemp, IsActive)
+ SELECT 1 AS Id, 'RADIO' AS QuestionType, 'Test' AS QuestionJsonTemp, 1 AS IsActive UNION ALL  
+ SELECT 2 AS Id, 'CHECK-BOX' AS QuestionType, 'Test' AS QuestionJsonTemp, 1 AS IsActive UNION ALL  
+ SELECT 3 AS Id, 'TEXT' AS QuestionType, 'Test' AS QuestionJsonTemp, 1 AS IsActive UNION ALL  
+ SELECT 4 AS Id, 'RATING' AS QuestionType, 'Test' AS QuestionJsonTemp, 1 AS IsActive 
+ BEGIN TRY
+  BEGIN TRANSACTION
+
+   MERGE TemplateDatbase.Ref.QuestionTypeTemplate AS TARGET
+   USING #QuestionTypeTemplate AS SOURCE
+      ON (TARGET.Id = SOURCE.Id)
+
+	  WHEN MATCHED AND (TARGET.QuestionType <> SOURCE.QuestionType OR TARGET.QuestionJsonTemp <> SOURCE.QuestionJsonTemp OR TARGET.IsActive <> SOURCE.IsActive)
+	  THEN
+	  UPDATE 
+	  SET TARGET.QuestionType = SOURCE.QuestionType,
+	      TARGET.QuestionJsonTemp <> SOURCE.QuestionJsonTemp,
+	      TARGET.IsActive = SOURCE.IsActive
+
+      WHEN NOT MATCHED BY TARGET THEN
+	  INSERT(Id,QuestionType,QuestionJsonTemp,IsActive)
+	  VALUES (SOURCE.Id, SOURCE.QuestionType, SOURCE.QuestionJsonTemp, SOURCE.IsActive);
+
+	  COMMIT TRANSACTION
+END TRY
+
+BEGIN CATCH
+   ROLLBACK TRANSACTION
+   THROW
+END CATCH
+GO
 
